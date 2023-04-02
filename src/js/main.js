33db032e9fd1fc79e59eeb07f62603de8ff19bba
Boris Galac <boris.galac@gmail.com>
2023-04-03 00:56:02 +0200

    update

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+///// TYPING EFFECT
+
+var typed = new Typed(".auto-type", {
+  strings: ["UX/UI designer", "Web developer", "Graphic designer"],
+  typeSpeed: 100,
+  backSpeed: 40,
+  loop: true,
+});
+
+console.log("javascript");
