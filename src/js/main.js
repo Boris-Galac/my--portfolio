2bd1ffe095efef8cb6edb6d6d3d5539e852a0388
Boris Galac <boris.galac@gmail.com>
2023-04-04 19:37:52 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 ///// TYPING EFFECT
 
-var typed = new Typed(".auto-type", {
-  strings: ["UX/UI designer", "Web developer", "Graphic designer"],
-  typeSpeed: 100,
-  backSpeed: 40,
-  loop: true,
-});
+if (document.querySelector(".home-main")) {
+  var typed = new Typed(".auto-type", {
+    strings: ["UX/UI designer", "Web developer", "Graphic designer"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+  });
+}
 
 ////// SCROLL TO TOP
 
